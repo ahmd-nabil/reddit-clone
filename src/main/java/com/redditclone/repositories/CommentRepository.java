849260092863d00bae0178285821078a8a3e0cc0
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-04-05 17:00:01 +0200

    Add Main Repositories

---
+++ src/main/java/com/redditclone/repositories/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.redditclone.repositories;
+
+import com.redditclone.entities.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
---
+++ src/main/java/com/redditclone/repositories/VoteRepository.java
@@ -0,0 +1,9 @@
+package com.redditclone.repositories;
+
+import com.redditclone.entities.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+}
