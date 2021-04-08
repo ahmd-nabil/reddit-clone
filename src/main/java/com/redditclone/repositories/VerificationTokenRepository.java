c6f450f067853ee64da7e82272ed5e5da9e8e6c0
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-04-08 02:20:49 +0200

    Create and Save VerificationToken for each new user

---
+++ src/main/java/com/redditclone/repositories/VerificationTokenRepository.java
@@ -0,0 +1,12 @@
+package com.redditclone.repositories;
+
+import com.redditclone.entities.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    Optional<VerificationToken> findVerificationTokenByToken(String token);
+}
--- src/main/java/services/AuthService.java
+++ src/main/java/services/AuthService.java
@@ -2,13 +2,17 @@ package services;
 
 import com.redditclone.dto.SignupRequest;
 import com.redditclone.entities.User;
+import com.redditclone.entities.VerificationToken;
 import com.redditclone.repositories.UserRepository;
+import com.redditclone.repositories.VerificationTokenRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -17,17 +21,24 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final EmailValidator emailValidator;
+    private final VerificationTokenRepository verificationTokenRepository;
 
     @Transactional
     public void signup(SignupRequest signupRequest) {
         validateSignupRequest(signupRequest);
+        // Create a valid user
         User user = new User();
         user.setUsername(signupRequest.getUsername());
         user.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
         user.setEmail(signupRequest.getEmail());
         user.setCreatedTime(Instant.now());
         user.setEnabled(false);
+        //create verification token for that user
+        VerificationToken verificationToken = generateVerificationToken(user);
+
+        // save user token in DB (order matters)
         userRepository.save(user);
+        verificationTokenRepository.save(verificationToken);
     }
 
     private void validateSignupRequest(SignupRequest signupRequest) {
@@ -39,5 +50,14 @@ public class AuthService {
         User oldUser = userRepository.findByEmail(signupRequest.getEmail()).orElse(null);
         if(oldUser != null)
             throw new IllegalStateException("There is already an account with this email");
+    }
+
+    private VerificationToken generateVerificationToken(User user) {
+        VerificationToken verificationToken = VerificationToken.builder()
+                                                    .token(UUID.randomUUID().toString())
+                                                    .user(user)
+                                                    .expiryDate(Instant.now().plus(30, ChronoUnit.MINUTES))
+                                                    .build();
+        return verificationToken;
     }
 }
