bac22f25e36d07b50328f273ea888144c4a5d20a
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-04-08 02:49:00 +0200

    Add MailService to send mails with JavaMailSender

---
+++ src/main/java/services/MailService.java
@@ -0,0 +1,32 @@
+package services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+@AllArgsConstructor
+public class MailService {
+
+    private final JavaMailSender mailSender;
+
+    @Async
+    public void sendMail(String to, String mail) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, "utf-8");
+            helper.setText(mail, true);
+            helper.setTo(to);
+            helper.setSubject("Activate your Account");
+            helper.setFrom("ahmednabil@reddit-clone.com");
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            throw new IllegalStateException("failed to send email");
+        }
+    }
+}
