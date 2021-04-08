b4962bdcf2ce9c2e05a91bb06ab556fc2c9bf4db
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-04-08 02:08:44 +0200

    Update SecurityConfig added PasswordEncoderBean

--- src/main/java/com/redditclone/security/SecurityConfig.java
+++ src/main/java/com/redditclone/security/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.redditclone.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -16,5 +20,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll()
                 .anyRequest()
                     .authenticated();
+    }
+
+    @Bean
+    protected PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
