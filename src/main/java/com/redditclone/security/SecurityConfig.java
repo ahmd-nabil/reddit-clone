bd3744b57cc77fd095d15fc043c57c764b45610d
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-04-11 08:23:19 +0200

    Add EmailAndPasswordAuthenticationFilter that generates jwt header for successful authentication

--- src/main/java/com/redditclone/security/SecurityConfig.java
+++ src/main/java/com/redditclone/security/SecurityConfig.java
@@ -1,36 +1,42 @@
 package com.redditclone.security;
 
+import com.redditclone.jwt.JwtConfig;
+import com.redditclone.jwt.JwtEmailAndPasswordAuthFilter;
 import com.redditclone.services.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @AllArgsConstructor
 @EnableWebSecurity
+@EnableConfigurationProperties
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
+    private final JwtConfig jwtConfig;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf()
                     .disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new JwtEmailAndPasswordAuthFilter("/api/auth/login",authenticationManager(), jwtConfig), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/api/auth/**")
                     .permitAll()
                 .anyRequest()
-                    .authenticated()
-                .and()
-                .formLogin()
-                    .loginPage("/api/auth/login");
+                    .authenticated();
     }
 
     @Override
@@ -49,11 +55,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     protected PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
     }
 }
