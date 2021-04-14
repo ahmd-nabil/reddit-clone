784a18ff84e7e8ffdcedee1b9c7195419c3cce35
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-04-14 19:59:17 +0200

    Add findBySubreddit

--- src/main/java/com/redditclone/repositories/PostRepository.java
+++ src/main/java/com/redditclone/repositories/PostRepository.java
@@ -1,9 +1,15 @@
 package com.redditclone.repositories;
 
 import com.redditclone.entities.Post;
+import com.redditclone.entities.Subreddit;
+import com.redditclone.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByUser(User user);
+    List<Post> findAllBySubreddit(Subreddit subreddit);
 }
