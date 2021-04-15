9558e1bac0d4a13e63277c08688fc1ea2f51ee48
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-04-15 12:55:38 +0200

    Add @Component

--- src/main/java/com/redditclone/converters/CommentToCommentDto.java
+++ src/main/java/com/redditclone/converters/CommentToCommentDto.java
@@ -2,8 +2,12 @@ package com.redditclone.converters;
 
 import com.redditclone.dto.CommentDto;
 import com.redditclone.entities.Comment;
+import lombok.AllArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+@Component
+@AllArgsConstructor
 public class CommentToCommentDto implements Converter<Comment, CommentDto> {
     @Override
     public CommentDto convert(Comment comment) {
