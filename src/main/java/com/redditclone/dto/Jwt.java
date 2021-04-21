3d6208ea0e56853e44f10a84094049f02f80e347
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-04-21 23:33:56 +0200

    simple jwt to expose to frontend

---
+++ src/main/java/com/redditclone/dto/Jwt.java
@@ -0,0 +1,12 @@
+package com.redditclone.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Jwt {
+    private String token;
+}
