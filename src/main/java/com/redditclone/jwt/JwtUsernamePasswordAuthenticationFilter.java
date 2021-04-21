bb0a74233120802054a4d26aad259ca7e1c4b7ff
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-04-21 23:33:16 +0200

    Using AuthService instead authentication filter to be able to return jwt token to frontend

--- src/main/java/com/redditclone/jwt/JwtUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/redditclone/jwt/JwtUsernamePasswordAuthenticationFilter.java
@@ -1,50 +1,38 @@
 package com.redditclone.jwt;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.redditclone.dto.LoginRequest;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 public class JwtUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-
-    private final JwtConfig jwtConfig;
-
-    public JwtUsernamePasswordAuthenticationFilter(AuthenticationManager authenticationManager, JwtConfig jwtConfig, String url) {
-        super.setAuthenticationManager(authenticationManager);
-        this.jwtConfig = jwtConfig;
-        setFilterProcessesUrl(url);
-    }
-
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        try {
-            LoginRequest loginRequest = new ObjectMapper()
-                    .readValue(request.getInputStream(), LoginRequest.class);
-            Authentication usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword());
-            Authentication authenticationResult = super.getAuthenticationManager().authenticate(usernamePasswordAuthenticationToken);
-            return authenticationResult;
-        } catch (IOException e) {
-            throw new IllegalStateException("Not Authenticated, cant read email and password");
-        }
-    }
-
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) {
-        String jwtToken = jwtConfig.generateToken(authResult);
-        response.addHeader(jwtConfig.getHeader(), jwtConfig.getTokenPrefix() + jwtToken);
-    }
-
-    @Override
-    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
-        response.addHeader(jwtConfig.getHeader(), "failure");
-    }
+//
+//    private final JwtConfig jwtConfig;
+//
+//    public JwtUsernamePasswordAuthenticationFilter(AuthenticationManager authenticationManager, JwtConfig jwtConfig, String url) {
+//        super.setAuthenticationManager(authenticationManager);
+//        this.jwtConfig = jwtConfig;
+//        setFilterProcessesUrl(url);
+//    }
+//
+//    @Override
+//    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+//        try {
+//            LoginRequest loginRequest = new ObjectMapper()
+//                    .readValue(request.getInputStream(), LoginRequest.class);
+//            Authentication usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword());
+//            Authentication authenticationResult = super.getAuthenticationManager().authenticate(usernamePasswordAuthenticationToken);
+//            return authenticationResult;
+//        } catch (IOException e) {
+//            throw new IllegalStateException("Not Authenticated, cant find email and password");
+//        }
+//    }
+//
+//    @Override
+//    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) {
+//        String jwtToken = jwtConfig.generateToken(authResult);
+//        response.addHeader(jwtConfig.getHeader(), jwtConfig.getTokenPrefix() + jwtToken);
+//    }
+//
+//    @Override
+//    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+//        response.addHeader(jwtConfig.getHeader(), "failure");
+//    }
 }
