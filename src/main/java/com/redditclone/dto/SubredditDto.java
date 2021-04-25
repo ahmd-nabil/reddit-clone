89d3b4035da590ff48412926410e7f66e03b695d
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-04-25 16:14:05 +0200

    add @NoArgsConstructor. fix bug cant construct this dto

--- src/main/java/com/redditclone/dto/SubredditDto.java
+++ src/main/java/com/redditclone/dto/SubredditDto.java
@@ -3,9 +3,11 @@ package com.redditclone.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class SubredditDto {
     private Long id;
