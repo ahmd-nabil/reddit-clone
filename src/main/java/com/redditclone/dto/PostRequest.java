aa222c639a3f9fc66047e298e227ec8a62859e6e
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-04-29 16:41:29 +0200

    fix a bug. add @NoArgsConstructor

--- src/main/java/com/redditclone/dto/PostRequest.java
+++ src/main/java/com/redditclone/dto/PostRequest.java
@@ -2,8 +2,10 @@ package com.redditclone.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class PostRequest {
     private Long id;
